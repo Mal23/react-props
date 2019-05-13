d76b7a520c10bb877a1ff773c0e23998fee73cf2
Mallarie <mallariemilano@gmail.com>
2019-05-13 16:14:38 -0700

    App component uses Dog

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Dog from './Dog';
+
+export default function App() {
+  console.log('hi');
+  return (
+    <>
+      <Dog name="Nan" age="5" weight="7 lbs" />
+    </>
+  );
+}
