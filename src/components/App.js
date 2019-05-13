33986c80e5748fdf445735ecc9fe1a6d7587d267
Mallarie <mallariemilano@gmail.com>
2019-05-13 16:33:51 -0700

    Added header to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Dog from './Dog';
+import Header from './Header';
 
 export default function App() {
   console.log('hi');
   return (
     <>
+      <Header />
       <Dog name="Nan" age="5" weight="7 lbs" />
     </>
   );
